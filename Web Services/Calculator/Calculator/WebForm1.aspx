67c84d73ebf17225b1cfd7303c3dc12a74f0141f
vasinanis <sinanis2002@gmail.com>
2024-11-09 19:22:32 +0200

    Add files via upload

---
+++ Web Services/Calculator/Calculator/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Calculator</h1>
+            <br />
+             Number 1:  <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+             <div>&nbsp;</div>
+            <asp:Button ID="Button1" runat="server" Text="+" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="-" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="*" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="/" OnClick="Button4_Click" />
+            <asp:Button ID="Button5" runat="server" Text="Clear" OnClick="Button5_Click" />
+            <br />
+            <div>&nbsp;</div>
+            Number 2:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+             <div>&nbsp;</div>
+            <asp:Label ID="Label1" runat="server" Text="Result:"></asp:Label>
+
+        </div>
+        <div>&nbsp;</div>
+        
+       
+    </form>
+</body>
+</html>
---
+++ Web Services/Calculator/Calculator/WebForm1.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        com.dneonline.www.Calculator myWS = new com.dneonline.www.Calculator();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a, b, apot;
+            a = Convert.ToInt32(TextBox1.Text);
+            b = Convert.ToInt32(TextBox2.Text);
+            apot = myWS.Add(a, b);
+            Label1.Text = Convert.ToString(apot);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int a, b, apot;
+            a = Convert.ToInt32(TextBox1.Text);
+            b = Convert.ToInt32(TextBox2.Text);
+            apot = myWS.Subtract(a, b);
+            Label1.Text = Convert.ToString(apot);
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int a, b, apot;
+            a = Convert.ToInt32(TextBox1.Text);
+            b = Convert.ToInt32(TextBox2.Text);
+            apot = myWS.Multiply(a, b);
+            Label1.Text = Convert.ToString(apot);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            int a, b, apot;
+            a = Convert.ToInt32(TextBox1.Text);
+            b = Convert.ToInt32(TextBox2.Text);
+
+            if (b != 0)
+            {
+                apot = myWS.Divide(a, b);
+                Label1.Text = Convert.ToString(apot);
+            }
+            else if (a != 0)
+                Label1.Text = "Ιmpossible";
+            else
+                Label1.Text = "Vagueness";
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+
+
+
+        }
+    }
+}
